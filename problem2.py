c7a279ae600cf248611cbefa4f5fe4a6759492dd
Sanjay Kumar Kalwa <sanjaykumarkalwa@Sanjays-MacBook-Air.local>
2023-10-11 00:22:22 +0530

    feat(): added solution for problem 2

---
+++ problem2.py
@@ -0,0 +1,10 @@
+def fn:
+    number = int(input())
+    number = str(n)
+    reverseNumber = number[::-1]
+    if (reverseNumber === number):
+        print(true)
+
+    else
+        print(false)
+fn()
