daf4e29465990f7811948c98a374cf05dc5b2585
Ps <ps.world.is.here@gmal.com>
2023-10-11 00:02:20 +0530

    Finding sum of digits in python

---
+++ sum_of_digits.py
@@ -0,0 +1,12 @@
+num = int(input())
+
+def sum_of_digits(num):
+  sum = 0
+  while num != 0:
+    digit = int(num % 10)
+    sum += digit
+    num = num / 10
+  return sum
+
+
+print(sum_of_digits(num))
